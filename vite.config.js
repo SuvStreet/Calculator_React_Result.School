d5ac04b681d1360d4f6f57bfc113e58e1249fb93
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-02-09 23:38:16 +0300

    edit file config vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  build: {
-    outDir: 'public', // имя общей выходной директории
-    assetsDir: 'js' // имя директории со статикой
-  },
   plugins: [react()],
   server: {
     open: true,
